f0fc183b6b6c00767682ebe99f1703a6ddb5c2a6
ethemcastle <ethemkalaja15@gmail.com>
2026-05-16 11:44:37 +0200

    first commit

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaLibSql({ url: "file:./dev.db" });
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg(connectionString);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
